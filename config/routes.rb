e1717702331da0767c1acbea12da913e05e7240e
zzz6519003 <zhengzhongzhao@gmail.com>
2013-11-29 22:56:11 +0800

    create users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 IGame::Application.routes.draw do
+  get "sessions/new"
+  get "sign_up" => "users#new", :as => "sign_up"
+  root :to => "users#new"
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

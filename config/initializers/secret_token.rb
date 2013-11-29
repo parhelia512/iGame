5469c926f8fbc629d2bfc17f25205c2054bfb422
zzz6519003 <zhengzhongzhao@gmail.com>
2013-11-29 22:20:32 +0800

    adf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IGame::Application.config.secret_key_base = 'bdc275c265e33abbf448ec47af1b9ce43a2a16744ad1e7265aac6846c6b7c2c85b9ab100f88f7824f0b4e64d129a9ed80b316b10c19c5cc5ae5e6c1d7c44aa33'

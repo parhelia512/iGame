232ecd5eeab3caa08cb6442b02493c8b2ea0649b
zzz6519003 <zhengzhongzhao@gmail.com>
2013-12-17 11:10:55 +0800

    squarsh

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 IGame::Application.routes.draw do
+  resources :games
+
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131129142411) do
+ActiveRecord::Schema.define(version: 20131217025045) do
+
+  create_table "games", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email"
